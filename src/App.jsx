dafbc4c54a77739f6606287fcb4a2643a9d094ee
MAHESH SINGH <richa.singhnxt213@gmail.com>
2022-12-31 19:11:22 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import Main from './Mycomponents/Main';
+import './App.css';
+
+function App() {
+
+ const [img, setImg]=useState(null);
+  const handleChange=(e)=>{
+      if (e.target.files && e.target.files[0]) {
+        setImg(URL.createObjectURL(e.target.files[0]))
+      }
+  }
+    
+  return (
+    <div className="App"> 
+    <form className='main-form'>
+      <input type="file" onChange={handleChange} className="choose"/>
+      <Main src={img} />
+     </form>
+     
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Mycomponents/Main.jsx
@@ -0,0 +1,68 @@
+import * as faceapi from "face-api.js";
+import { useEffect, useRef, useState } from 'react';
+import './Main.css';
+function Main(props) {
+    
+    function handleClick(e){
+        console.log("hi")
+    }
+  const imageRef=useRef();
+  const canvasRef=useRef();
+
+  const handleImg= async()=>{
+    
+    const detections=await faceapi.detectAllFaces(imageRef.current,
+      new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+     
+      console.log(canvasRef.current);
+      
+      canvasRef.current.innerHtml=faceapi.createCanvasFromMedia(imageRef.current);
+      faceapi.matchDimensions(canvasRef.current, {
+        width: 700,
+        height: 400
+      })
+       const  resized=faceapi.resizeResults(detections,{
+        width: 700,
+        height: 400
+       })
+      faceapi.draw.drawDetections(canvasRef.current, resized);
+      faceapi.draw.drawFaceExpressions(canvasRef.current, resized);
+      faceapi.draw.drawFaceLandmarks(canvasRef.current, resized);
+      
+  }
+  useEffect(()=>{
+  const loadModels=()=>{
+  Promise.all([
+    faceapi.nets.ssdMobilenetv1.loadFromUri("/models"),
+    faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+    faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+    faceapi.nets.faceExpressionNet.loadFromUri("/models")
+    
+  ]).then(handleImg)
+  .catch(console.log("false"))
+  }
+
+  imageRef.current && loadModels()
+  
+},[props.src])
+
+  return (
+    <div className="Main"> 
+    
+    <div className="img">
+    <img ref={imageRef} src={props.src}/>
+     
+     <canvas ref={canvasRef} width="700"
+     height="400"/>
+    </div>
+      
+    
+    <form>
+    <button onClick={handleClick} className="button">New Picture</button>
+    </form>
+    </div>
+    
+  );
+}
+
+export default Main;
